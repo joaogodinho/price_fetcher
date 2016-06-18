ee015ff9095bdaa1f2d72fa3824e254a34eb7d5b
joaogodinho <joaocfgodinho@gmail.com>
2016-06-18 19:53:03 +0100

    Finished pcdiga spider.

--- price_fetcher/spiders/pcdiga.py
+++ price_fetcher/spiders/pcdiga.py
@@ -11,12 +11,14 @@ class PCDigaSpider(scrapy.Spider):
     start_urls = [
         'https://www.pcdiga.com/nm_pesquisa.php?pesquisa=%&pagina=1&ordem=4&categoria=&marca=',
     ]
-    pn_regex = re.compile(r'\((.+)\)')
+    pn_regex = re.compile(r'\(([^)]+)\)$')
 
     def parse(self, response):
+        continue_scraping = True
         for sel in response.xpath('//table[@height="170px"]'):
             item = ProductItem()
-            item['name'] = sel.xpath('.//a[@class="prod"]/text()').extract_first()
+            item['url'] = sel.xpath('.//a/@href').extract_first().split('?')[0]
+            item['name'] = sel.xpath('.//a[@class="prod"]/text()').extract_first().strip()
 
             pn_search = self.pn_regex.search(item['name'])
             if pn_search is not None:
@@ -25,9 +27,19 @@ class PCDigaSpider(scrapy.Spider):
                 item['part_number'] = None
 
             item['price'] = sel.xpath('.//td[@class="preco"]/text()').extract()[-1].replace('€', '').replace('.', '').replace(',', '.').strip()
-            yield item
+            if float(item['price']) < 20.0:
+                continue_scraping = False
 
-        # pages = response.xpath('//a[@class="pageResults"]/@href')
-        # for href in pages[:math.ceil(len(pages)/2)]:
-        #     url = response.urljoin(href.extract()).split('&osCsid')[0]
-        #     yield scrapy.Request(url)
+            on_sale = sel.xpath('.//parent::td/div/div/img/@src').extract_first()
+            if on_sale is not None:
+                item['on_sale'] = True
+            else:
+                item['on_sale'] = False
+
+            if continue_scraping:
+                yield item
+
+        if continue_scraping:
+            pages = response.xpath('//a[@class="cinza"]/@href')
+            for href in pages[:math.ceil(len(pages)/2)]:
+                yield scrapy.Request(href.extract())
