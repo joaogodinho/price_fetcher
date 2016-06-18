07e44d195adea65a23906cfb54cfac31e79dfc0b
joaogodinho <joaocfgodinho@gmail.com>
2016-06-18 19:54:23 +0100

    Removed referer middleware.

--- price_fetcher/settings.py
+++ price_fetcher/settings.py
@@ -46,9 +46,9 @@ COOKIES_ENABLED = False
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'price_fetcher.middlewares.MyCustomSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapy.spidermiddlewares.referer.RefererMiddleware': None,
+}
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
